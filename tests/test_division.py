e2f561d258d6be1b80ca3ee64e992258f37fe1fb
Азат Янбердин <azat@MacBook-Air.local>
2025-05-07 15:13:04 +0500

    add parametrize in project

---
+++ tests/test_division.py
@@ -0,0 +1,22 @@
+from utils import division
+import pytest 
+
+
+
+@pytest.mark.parametrize("a, b, expected_result", [(10, 2, 5),
+                                                   (20, 10, 2),
+                                                   (5, 2, 2.5),])
+def test_division(a, b, expected_result):
+    assert division(a, b) == expected_result
+    
+    
+@pytest.mark.parametrize("expected_exception, divider, divisionable", [(ZeroDivisionError, 0, 10),
+                                                         (TypeError, "2", 20)])
+def test_division_zero(expected_exception, divider, divisionable):
+    with pytest.raises(expected_exception):
+        division(divisionable, divider)
+    
+
+def test_division_type_error():
+    with pytest.raises(TypeError):
+        division(19, "2")    
